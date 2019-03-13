64cd656b8ff174f08cccea5799c8ade32102f0a8
w3bgir1 <akulshyna@gmail.com>
2019-03-13 12:15:25 +0100

    added secret endpoint

--- auth/routes.js
+++ auth/routes.js
@@ -3,7 +3,7 @@ const { toJWT , toData } = require('./jwt')
 const router = new Router()
 
 
-router.post('/logins', (req, res, next) => {
+router.post('/logins', (req, res) => {
     const data = req.body
 
     if (!data.email || !data.password) {
@@ -14,6 +14,29 @@ router.post('/logins', (req, res, next) => {
         return res.send({
             jwt: toJWT({ userId: 1 })
           })
+})
+
+router.get('/secret-endpoint', (req, res) => {
+    const auth = req.headers.authorization && req.headers.authorization.split(' ')
+    if (auth && auth[0] === 'Bearer' && auth[1]) {
+      try {
+        const data = toData(auth[1])
+        res.send({
+          message: 'Thanks for visiting the secret endpoint.',
+          data
+        })
+      }
+      catch(error) {
+        res.status(400).send({
+          message: `Error ${error.name}: ${error.message}`,
+        })
+      }
+    }
+    else {
+      res.status(401).send({
+        message: 'Please supply some valid credentials'
+      })
+    }
 })
   
 
